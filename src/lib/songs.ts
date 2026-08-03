c610785bbe31b59de2c6ee0f264c71d7e410fedd
kim-hyunjin <hyunjin1612@gmail.com>
2026-08-03 15:13:16 +0900

    feat: 단원 페이지에 유튜브 플레이어 추가

--- src/lib/songs.ts
+++ src/lib/songs.ts
@@ -5,6 +5,7 @@ export interface SongDefinition {
   artist: string;
   kicker: string;
   description: string;
+  youtubeVideoId: string;
 }
 
 export const songs: SongDefinition[] = [
@@ -16,6 +17,7 @@ export const songs: SongDefinition[] = [
     kicker: '상처마저 사랑하게 되는 시간',
     description:
       '상처를 피하던 마음이 함께 쌓은 시간과 기억을 사랑하게 되는 흐름을 따라갑니다.',
+    youtubeVideoId: 'cMLTX2FClxw',
   },
   {
     slug: 'nichijo',
@@ -25,6 +27,7 @@ export const songs: SongDefinition[] = [
     kicker: '지친 하루를 알아봐 주는 안부',
     description:
       '성과로 평가받는 일상의 무게와, 때맞춰 건네온 안부가 주는 위로를 따라갑니다.',
+    youtubeVideoId: 'LbtQM793jn8',
   },
 ];
 
